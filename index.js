498ecc374899272a0d6ef1c00a77b91121f77f8e
Quang Huy <57585160+QuangHuy161@users.noreply.github.com>
2021-09-10 11:19:07 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('/public'));
+app.use(express.static('public'));
 
 app.get('/public', (req, res) => {
     res.sendFile('index.html')
